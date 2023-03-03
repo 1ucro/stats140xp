de8b19dcc45e7950d51b047f28792ce056ceb052
Lucas <96842464+1ucro@users.noreply.github.com>
2023-03-02 18:46:32 -0800

    Add files via upload

---
+++ x02_prep_auditor_data.R
@@ -0,0 +1,33 @@
+#==============================================================================
+# File           :  x02_prep_auditor_data
+# Purpose        :  Load and clean CA auditor data (FY 2016-2020)
+# Notes          :  https://www.auditor.ca.gov/local_high_risk/dashboard-csa
+# Last modified  :  03/02/2023
+#==============================================================================
+
+# 1. Load data ---------------------------------------------------------------
+# Data from fiscal year 2016-2017
+fiscal_17 <- cbind(read_csv("./data/auditor_data/fy2016-17.csv"), "end_year" = 2017)
+
+# Data from fiscal year 2017-2018
+fiscal_18 <- cbind(read_csv("./data/auditor_data/fy2017-18.csv"), "end_year" = 2018)
+
+# Data from fiscal year 2018-2019
+fiscal_19 <- cbind(read_csv("./data/auditor_data/fy2018-19.csv"), "end_year" = 2019)
+
+# Data from fiscal year 2019-2020
+fiscal_20 <- cbind(read_csv("./data/auditor_data/fy2019-20.csv"), "end_year" = 2020)
+
+# 2. Merge data --------------------------------------------------------------
+# Note: Not all cities are present in each year
+auditor_data <- rbind(fiscal_17, fiscal_18, fiscal_19, fiscal_20)
+names(auditor_data) <- tolower(names(auditor_data))
+
+# 3. Remove redundant variables -------------------------------------------
+auditor_data <- auditor_data |> select(!ends_with("rank"))
+auditor_risk <- auditor_data |> select(overall_risk, overall_points)
+auditor_data <- auditor_data |> select(!ends_with("risk")) |> select(!ends_with("points"))
+auditor_data <- data.frame("city_name" = auditor_data$city_name, auditor_risk, auditor_data[, -1])
+
+# 4. Clean workspace ---------------------------------------------------------
+rm(fiscal_17, fiscal_18, fiscal_19, fiscal_20, auditor_risk)
---
+++ x04_prep_final_data.R
@@ -0,0 +1,44 @@
+#==============================================================================
+# File           :  x04_prep_voter_data
+# Purpose        :  Merge voting, demographic, and auditor data
+# Notes          :
+# Last modified  :  03/02/2023
+#==============================================================================
+
+# 1. Construct city-county dictionary -------------------------------------
+# Source: https://bythenumbers.sco.ca.gov/Raw-Data/Cities-Raw-Data-for-Fiscal-Years-2020-21/kyrq-f99p
+city_to_county <- read_excel("data/city_to_county_data/CIX_EachDataSet_2020-21_20221009_V9.xlsx")
+
+# Streamline column names
+names(city_to_county) <- str_replace_all(string       =  names(city_to_county)
+                                       , pattern      =  " "
+                                       , replacement  =  "\\_") |> tolower()
+
+# Retain relevant variables
+city_to_county <- city_to_county |> select(city, county_name, zip)
+
+# Rename city column to city_name to serve as merger key
+names(city_to_county)[names(city_to_county) == "city"] <- "city_name"
+
+# 2. Add counties and zip to auditor data ---------------------------------
+final_data <- left_join(auditor_data, city_to_county, by = "city_name")
+final_data <- left_join(final_data, county_voter_reg, by = "county_name")
+
+# 3. Add demographic variables to auditor data ----------------------------
+## 3.1 Area median income (AMI) by county ---------------------------------
+# Source: https://data.ca.gov/dataset/income-limits-by-county
+county_income <- read_csv("data/census_data/2022-income-limits.csv") |>
+  select(County, AMI)
+final_data <- left_join(final_data, county_income, by = c("county_name" = "County"))
+
+# In-progress -------------------------------------------------------------
+
+# Local business patterns
+# Add demographic variables by zipcode
+# industry_zip <- read.table("data/census_demographics/zbp20detail.txt")
+#
+# # Add demographic variables by county
+# industry_county <- read.table("data/census_demographics/cbp20co.txt")
+
+# 5. Clean workspace ------------------------------------------------------
+rm(city_to_county, county_income, county_voter_reg, auditor_data)
